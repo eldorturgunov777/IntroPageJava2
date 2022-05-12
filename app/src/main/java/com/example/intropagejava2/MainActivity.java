564d38188f796047a4528871d9f603c11a2288dc
eldorturgunov777 <eldorturgunov777@gmail.com>
2022-05-12 10:35:17 +0500

    lates

--- app/src/main/java/com/example/intropagejava2/MainActivity.java
+++ app/src/main/java/com/example/intropagejava2/MainActivity.java
@@ -37,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
             startActivity(mainActivity);
             finish();
         });
+
         tabIndicator = findViewById(R.id.tab_indicator);
         btnSkip.setOnClickListener(view -> recyclerView.getRecycledViewPool());
         tabIndicator.addOnTabSelectedListener(new TabLayout.BaseOnTabSelectedListener() {
